12de4b388efdfd4aa3e75f8e63dea0e6d7beac6a
Furajkova Alena <Furajkova@mail.ru>
2024-06-08 04:27:11 +0300

    домашние задание

--- js/main.js
+++ js/main.js
@@ -1,115 +1,93 @@
-//Задания от 5.06
+// задание 1
 
-//задание 1
-
-age = prompt('Введите ваш возраст')
-function whoAreYou(age) {
-    if (age > 60) return 'вы пенсионер'
-    if (age > 18) return 'вы взрослый'
-    if (age > 12) return 'вы подросток'
-    return 'Вы ребенок'
-}
-alert(whoAreYou(age))
-
-//задание 2
-
-(function printSpecialSymbol(a = prompt("Введите число от 0 до 9 для выхода оставьте поле пустым и нажмите OK")) {
-    if (a === "") return
-    const s = [')', '!', '@', '#', '$', '%', '^', '&', '*', '('][parseInt(a)]
-    switch (s) {
-        case undefined:
-            alert("ОШИБКА ВВОДА")
-            break;
-            default:
-                alert(s)
-    }
-    printSpecialSymbol()
-})()
-
-//задание 3
-
-let str = prompt('Введите число');
-str = Array.from(str);
-alert(str.some((v,i,a) => str.lastIndexOf(v) != i ) ? 'Есть одинаковые' : 'Нет одинаковых');
-
-//задание 4
-
-let leap_year1 = Math.trunc(
-    +prompt("Введите год для проверки на високосность: ")
-);
-alert(
-    !(leap_year1 % 400) || (!(leap_year1 % 4) && leap_year1 % 100)
-    ? "Високосный год!"
-    : "Невисокосный год!"
-)
-
-//задание 5
-
-let palindrom = 0;
-do {
-    palindrom = Math.abs(
-        +prompt("Введите пятиразрядное число для проверки на палиндром: ")
-    );
-} while (palindrom < 10000 || palindrom > 99999);
-let array = [];
-for (i = 0; i < 5; i++) {
-    array[i] = palindrom % 10;
-    palindrom = Math.trunc(palindrom / 10);
-}
-if (array[0] == array[4] && array[1] == array[3]) {
-    alert("Да, это число палиндром!");
-} else {
-    alert("Нет, введенное число не палиндром!");
-}
-
-//задание 6
-
-let summa = Math.abc(+prompt("Введите сумму USD для обмена: "));
-const eur = Number(1.08);
-const uan = Number(0.15);
-const azn = Number(0.59);
-let rate = "";
-do {
-    rate = prompt("На какую валюту вы хотите обменять доллары? Введите EUR для обмена на евро, UAN для обмена на юани, либо AZN для азербайджанских манатов.");
-} while (rate != "EUR" && rate != "UAN" && rate != "AZN");
-let convert_summa = 0;
-if (rate == "EUR") {
-    convert_summa = (summa * eur).toFixed(2);
-}
-if (rate == "UAN") {
-    convert_summa = (summa * uan).toFixed(2);
-}
-if (rate == "AZN") {
-    convert_summa = (summa * azn).toFixed(2);
-}
-alert(`Вы поменяли ${summa} USD на ${convert_summa} ${rate}`);
+// let userName = prompt("Как вас зовут?");
+// alert(`Привет ${ userName }`);
 
 
-//задание 7
+// задание 2
 
-let check_sale = Math.abs(+prompt("Введите сумму вашей покупки: "));
-let discount = Number(0);
-if (check_sale >= 200 && check_sale <= 300) {
-    discount = 3;
-    check_sale = check_sale - (check_sale * discount) / 100;
-}
-if (check_sale > 300 && check_sale <=500) {
-    discount = 5;
-    check_sale = check_sale - (check_sale * discount);
-}
-if (check_sale > 500) {
-    discount = 5;
-    check_sale = check_sale - (check_sale * discount);
-}
-alert(`Вы купили товары на сумму ${
-    check_sale / (1 - discount / 100)
-} рублей. Ваша скидка ${
-    discount
-}% - ${
-    ((check_sale / (1 - discount / 100)) * discount) / 100
-} рублей. Всего к оплате ${
-    check_sale
-} рублей.`);
+// let age = prompt('В каком году вы родились?');
+
+// const year = prompt('Какой сейчас год?');
+
+// alert(`В этом году вам будет ` + ( year - age ) + ' лет');
 
 
 
+// задание 3
+
+// let age = prompt('Введите длину стороны квадрата:');
+// alert(`Периметр квадрата равен: ` + 4 * age);
+
+
+
+// задание 4
+
+// let age = prompt('Введите радиус окружности: ');
+// let user = Math.PI * age ** 2;
+// alert(`Площадь окружности: ` + user);
+
+
+// задание 5
+
+// let age = prompt("Введите расстояние между городами в км: ");
+// let user = prompt("За какое время вы хотите преодолеть это расстояние в часах? ");
+// alert(`Вам необходимо двигаться со средней скоростью ` + age / user + ' км/ч');
+
+
+
+// задание 6
+
+// const user = prompt('Введите текущий курс доллара по отношению к евро: ');
+// let dollar = prompt('Введите количество долларов для обмена: ');
+// alert("По текущему курсу при обмене вы получите " + dollar * user + " евро");
+
+
+
+// задание 7
+
+// const GB_MB = 1024;
+// let flash = prompt('Каков объем вашей флешки в Гб? ')
+// let size = Math.trunc(flash * 1000 / GB_MB);
+// if (size < 0) {
+//     alert(`Ваша флешка перезаполнена, освободите пространство`);
+// } else {
+//     alert('На флешку поместится ' + size + ' файлов объемом 820 Мб')
+// };
+
+
+
+
+// задание 8
+
+// let user = prompt("Cколько у вас в бумажнике денег? ");
+// let price = prompt("И почем одна шоколадка? ");
+// user = Number(user);
+// price = Number(price);
+// let sell = Math.trunc(user / price);
+// let change = user - price * sell;
+// if (sell < 1) {
+//     alert('Сегодня вы на нуле. приходите в другой раз');
+// } else {
+//     alert('Получите свои ' + sell + ' шоколадки, а также сдачу ' + (user - price * sell) + ' рублей ');
+// }
+
+
+
+// задание 9
+
+// user = prompt('Введите трехзначное число: ');
+// let a = Number(user % 10);
+// let b = Number((user - a) / 10 % 10);
+// let c = Number((user - a - b * 10) / 100);
+// alert(`Перевертыш: ` + (a * 100 + b * 10 + c));
+
+
+
+
+// задание 10
+
+
+// let a = prompt('Введите число для проверки на четность: ');
+// a = Number(a);
+// alert((parseInt(a) % 2 == 0) ? "четное" : "нечетное");
