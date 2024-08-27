b74f1ce579b3ca7020c73a4ac2145fa36fe3ce1d
Furajkova Alena <furajkova@mail.ru>
2024-08-27 19:46:22 +0300

    домашнее

--- js/script.js
+++ js/script.js
@@ -1,15 +1,6 @@
-async function feme() {
-    fetch(`https://api.github.com/users/Alena152197`)
-    .then(response => response.json())
-    .then(obj => {
-        const date = new Date(obj.created_at)
-        const moths = [
-            "январь", "февраль", "март", "апрель", "май", "июнь", "июль", "август", "сентябрь", "октябрь", "ноябрь", "декабрь"
-        ]
-        console.log(
-            `Аккаун зарегистрирован ${date.getDate()}, ${moths[date.getMonth()]}, ${date.getFullYear()}`
-        );
-    })
-}
+let menuElem = document.getElementById('dessert');
+let titleElem = menuElem.querySelector('.title');
 
-feme(prompt(`введите пароль`))
+titleElem.onclick = function() {
+    menuElem.classList.toggle('open');
+};
