4a8df58fe4551d3c90ba54ad5a5a7763576c546c
Furajkova Alena <furajkova@mail.ru>
2024-08-27 21:19:09 +0300

    домашнее

--- js/script.js
+++ js/script.js
@@ -1,6 +1,38 @@
-let menuElem = document.getElementById('dessert');
-let titleElem = menuElem.querySelector('.title');
+// let menuElem = document.getElementById('dessert');
+// let titleElem = menuElem.querySelector('.title');
 
-titleElem.onclick = function() {
-    menuElem.classList.toggle('open');
+// titleElem.onclick = function() {
+//     menuElem.classList.toggle('open');
+// };
+
+
+
+//                  вторая задача               //
+
+let i = 1;
+
+for(let li of carusel.querySelectorAll('li')) {
+    li.style.position = 'relative';
+    li.insertAdjacentHTML('beforeend', `<span style="position:absolute;left:0;top:0">${i}</span>`);
+    i++;
+}
+
+let width = 130;
+let count = 3;
+
+let list = carusel.querySelector('ul');
+let listElems = carusel.querySelectorAll('li');
+
+let position = 0;
+
+carusel.querySelector('.levo').onclick = function() {
+    position += width * count;
+    position = Math.min(position, 0)
+    list.style.marginLeft = position + 'px';
+};
+
+carusel.querySelector('.pravo').onclick = function() {
+    position -= width * count;
+    position = Math.max(position, -width * (listElems.length - count));
+    list.style.marginLeft = position + 'px';
 };
